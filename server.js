88390f76cef5d76fb003b548f734320e0d5d6188
Michele <michele.carmagnani@gmail.com>
2020-04-01 16:52:57 +0200

    add first GET rout

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+
 const express = require('express');
 const app = express();
 
+
+const mochUserData=[
+    {name:'Marck'};
+    {name:'Jill'}
+]
+app.get('/user', function(req,res){
+    res.json({
+        success: true,
+        message:'succefully got users. Nice',
+        users:mockUserData
+    })
+})
 app.listen(8000,function(){
     console.log('Server is running')
 })
